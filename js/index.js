491f8f84c7840cf12666a17b20624480c1393823
Strelets-2 <strelets2021@yandex.ru>
2024-03-04 14:38:32 +1000

    версия до внесения iframe с фильмами через js

---
+++ js/index.js
@@ -0,0 +1,18 @@
+const init = () => {
+  const trailersContainer = document.querySelector('.trailers__container');
+  const trailersButtons = document.querySelectorAll('.trailers__button')
+
+  trailersButtons.forEach(btn => {
+    btn.addEventListener('click', () => {
+      trailersButtons.forEach(tBtn => {
+        if (tBtn === btn) {
+          tBtn.classList.add('trailers__button--active')
+        } else {
+          tBtn.classList.remove('trailers__button--active')
+        }
+      })
+    })
+  });
+};
+
+init();
